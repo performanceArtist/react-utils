fae8c957c504e5b41411c37d2f28958b8bcbb96f
megakaban <nakedant19@gmail.com>
2021-04-03 23:55:49 +0700

    Fix useObservable cleanup

--- src/useObservable.ts
+++ src/useObservable.ts
@@ -1,13 +1,10 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Observable } from 'rxjs';
 
 export const useObservable = <A>(fa: Observable<A>, initial: A): A => {
   const [value, setValue] = useState(initial);
-
-  useMemo(() => {
-    const subscription = fa.subscribe(setValue);
-    return () => subscription.unsubscribe();
-  }, []);
+  const subscription = useMemo(() => fa.subscribe(setValue), [fa]);
+  useEffect(() => () => subscription.unsubscribe(), [subscription]);
 
   return value;
 };
